48876f82039cf0a4d108809b4317a85bcfa646ea
siham binta habib <sihambintahabib@gmail.com>
2025-09-25 00:30:00 +0600

    file setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-
 import './App.css'
-
+import { ToastContainer, toast } from 'react-toastify';
 function App() {
-
+  toast('hi')
   return (
     <>
-      
-        <h1>Hi React</h1>
+      <h1>Hi React....with fun </h1>
+<nav>navication</nav>
+<main>
+  <section> display</section>
+  <section> card </section>
+  
+  </main>
+<footer>footer</footer>
 
-       
+<ToastContainer></ToastContainer>
     </>
   )
 }
